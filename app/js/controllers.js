3590c2af21f477d06320beb0a9eb1594227560b5
sjp20102020 <sjp20102020@gmail.com>
2016-01-01 17:04:12 +0000

    sjp commit

---
+++ app/js/controllers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+/* CONTROLLER TEST2 */
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+
+/* CONTROLLER TEST3 */
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };
+  }]);
---
+++ app/js/filters.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/*Filter TEST1*/
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
